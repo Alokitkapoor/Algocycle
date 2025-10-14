9712527b4a52ed5e015ef194a4a530ed3da2359b
AlokitKapoor <alokitkapoor6@gmail.com>
2025-10-14 21:51:49 +0530

    login overlay

--- public/js/overlay.js
+++ public/js/overlay.js
@@ -1,26 +1,59 @@
+// so that not both the overlays are 
+// shown simultaneously
+
+let flag = false; 
+
 const overlay = document.getElementById('overlay');
-      const closeOverlay = document.getElementById('closeOverlay');
-
-      // Select all Sign In buttons
-      const signInButtons = [
-        document.getElementById('signInHero'),
-        document.getElementById('signInNav'),
-        document.getElementById('signInMobile')
-      ];
-
-      signInButtons.forEach(btn => {
-        if (btn) {
-          btn.addEventListener('click', (e) => {
-            e.preventDefault();
+const closeOverlay = document.getElementById('closeOverlay');
+// Select all Sign In buttons
+const signInButtons = [
+  document.getElementById('signInHero'),
+  document.getElementById('signInNav'),
+  document.getElementById('signInMobile')
+];
+signInButtons.forEach(btn => {
+  if (btn) {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        // check the flag after the click has happended
+        if(!flag){
+            flag = true;
             overlay.classList.add('active');
-          });
         }
-      });
+    });
+  }
+});
+closeOverlay.addEventListener('click', () => {
+  overlay.classList.remove('active');
+  flag = false;
+});
 
-      closeOverlay.addEventListener('click', () => {
-        overlay.classList.remove('active');
-      });
 
-      overlay.addEventListener('click', (e) => {
-        if (e.target === overlay) overlay.classList.remove('active');
-      });
+// doing the same for login buttons ---------- >>
+// Select all Sign In buttons
+
+const oy = document.getElementById('oy');
+const coy = document.getElementById('coy');
+
+const loginbuttons = [
+  document.getElementById('lh'),
+  document.getElementById('ln'),
+  document.getElementById('lm')
+];
+
+loginbuttons.forEach(btn => {
+    if (btn) {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(!flag){
+                flag = true;
+                oy.classList.add('active');
+            }
+        });
+    }
+});
+
+coy.addEventListener('click', () => {
+  oy.classList.remove('active');
+  flag = false;
+});
